860f6f7577ec070a13e47ec9812e9c39dc415941
Phillip LeBlanc <phillip@leblanc.tech>
2024-04-01 18:59:50 +0900

    Fix

--- .github/actions/chart-releaser-installer/install.sh
+++ .github/actions/chart-releaser-installer/install.sh
@@ -6,32 +6,17 @@ DEFAULT_CHART_RELEASER_VERSION=v1.6.1
 
 main() {
   local version="$DEFAULT_CHART_RELEASER_VERSION"
-  local install_dir=
-
-  if [[ -z "$install_dir" ]]; then
-    local arch
-    arch=$(uname -m)
-    install_dir="$RUNNER_TOOL_CACHE/cr/$version/$arch"
-  fi
+  local install_dir="/usr/local/bin/"
 
   install_chart_releaser
 }
 
 install_chart_releaser() {
-  if [[ ! -d "$RUNNER_TOOL_CACHE" ]]; then
-    echo "Cache directory '$RUNNER_TOOL_CACHE' does not exist" >&2
-    exit 1
-  fi
-
-  if [[ ! -d "$install_dir" ]]; then
-    mkdir -p "$install_dir"
-
-    echo "Installing chart-releaser on $install_dir..."
-    curl -sSLo cr.tar.gz "https://github.com/helm/chart-releaser/releases/download/$version/chart-releaser_${version#v}_linux_amd64.tar.gz"
-    tar -xzf cr.tar.gz -C "$install_dir"
-    $install_dir/cr version
-    rm -f cr.tar.gz
-  fi
+  echo "Installing chart-releaser on $install_dir..."
+  curl -sSLo cr.tar.gz "https://github.com/helm/chart-releaser/releases/download/$version/chart-releaser_${version#v}_linux_amd64.tar.gz"
+  tar -xzf cr.tar.gz -C "$install_dir"
+  $install_dir/cr version
+  rm -f cr.tar.gz
 
   echo 'Adding cr directory to PATH...'
   export PATH="$install_dir:$PATH"
